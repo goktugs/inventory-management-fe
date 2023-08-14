de99ae934aad15fe316001dc313005498b24311a
goktugs <gokojapan5@gmail.com>
2023-08-14 16:45:55 +0300

    Initial Commit

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { LoginPage } from './pages/Login/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginPage />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LoginPage = () => {
+    return (
+        <div className='text-center text-9xl text-red-900'>LoginPage</div>
+    )
+}
