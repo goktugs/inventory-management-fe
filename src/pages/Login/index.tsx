d727badfe96e768f2607ea1f9d40e72520f4143b
goktugs <gokojapan5@gmail.com>
2023-08-15 10:04:57 +0300

    feat: add login page ui

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { useForm, SubmitHandler } from "react-hook-form"
 
-export const LoginPage = () => {
+type LoginFormData = {
+    username: string;
+    password: string;
+};
+
+export default function LoginPage() {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<LoginFormData>();
+
+    const onSubmit: SubmitHandler<LoginFormData> = data => console.log(data)
+
+
     return (
-        <div className='text-center text-9xl text-red-900'>LoginPage</div>
+
+        <main className='w-full h-screen flex items-center justify-center '>
+            <form className="flex flex-col gap-8 border py-10 px-20" onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex flex-col space-y-2" >
+                    <label htmlFor="username-input">Kullanıcı Adı</label>
+                    <input className="border border-slate-700 py-2 pl-4 w-80 " id="username-input" type="text" {...register("username", (
+                        {
+                            minLength: 3,
+                            maxLength: 10
+                        }
+                    ))} />
+                </div>
+                <div className="flex flex-col space-y-2" >
+                    <label htmlFor="password-input" >Şifre</label>
+                    <input className="border border-slate-700 py-2 pl-4 w-80 " id="password-input" type="password"  {...register("password")} />
+                </div>
+                {
+                    errors.username && <span>this filed is w</span>
+                }
+                <button type="submit" className="bg-slate-400 text-white py-4" >Gönder</button>
+            </form>
+        </main>
     )
+
+
 }
